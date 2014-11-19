26810076e05ebf9bf22cf9723845126a1933cdad
Mickroz <mike.kros@gmail.com>
2014-11-19 23:15:09 +0100

    added cache and curl function

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ echo '<html><head><title>What2Watch</title>
     .container {
         width: 500px;
         clear: both;
-		margin: 0 auto;
     }
 	.container img {
         width: 500px;
@@ -21,10 +20,10 @@ echo '<html><head><title>What2Watch</title>
 		margin: 0 auto;
 	}
 	fieldset.submit-buttons {
-	text-align: center;
-	vertical-align: middle;
-	margin: 5px 0;
-	border-width: 0;
+		text-align: center;
+		vertical-align: middle;
+		margin: 5px 0;
+		border-width: 0;
 	}
 	.header {
 		letter-spacing: 1px;
@@ -89,91 +88,153 @@ if (isset($_POST['submit']))
 
 if ($config)
 {
-	// We have a config, lets get started with grabbing all shows from sickbeard
-	$response = file_get_contents($sickbeard . "/api/" . $sb_api . "/?cmd=shows&sort=name");
-	$result = json_decode($response, true);
+	$cache_file = 'cached.json';
+	$cache_life = '1800'; //caching time, in seconds
+	$filemtime = @filemtime($cache_file);  // returns FALSE if file does not exist
+	if (!$filemtime or (time() - $filemtime >= $cache_life))
+	{
+		echo "<pre>";
+		// We have a config, lets get started with grabbing all shows from sickbeard
+		$response = curl($sickbeard . "/api/" . $sb_api . "/?cmd=shows&sort=name");
+		$result = json_decode($response, true);
 
-	foreach ($result['data'] as $show => $values)
-	{	
-		$show_id = file_get_contents($sickbeard . "/api/" . $sb_api . "/?cmd=show&tvdbid=" . $values['tvdbid']);
-		$result_show = json_decode($show_id, true);
-		// Checking which show actually has a episode downloaded
-		// and put all  tvdb id's in an array
-		// TODO grab naming pattern
-		$season_list = $result_show['data']['season_list'];
-		$shows[$values['tvdbid']]['show_name'] = $result_show['data']['show_name'];
-		foreach ($season_list as $id => $season)
+		foreach ($result['data'] as $show => $values)
+		{	
+			$show_id = curl($sickbeard . "/api/" . $sb_api . "/?cmd=show&tvdbid=" . $values['tvdbid']);
+			$result_show = json_decode($show_id, true);
+			// Checking which show actually has a episode downloaded
+			// and put all  tvdb id's in an array
+			// TODO grab naming pattern
+			$season_list = $result_show['data']['season_list'];
+			$shows[$values['tvdbid']]['show_name'] = $result_show['data']['show_name'];
+			foreach ($season_list as $id => $season)
+			{
+				$padded = sprintf('%02d', $season); 
+				$dir = $result_show['data']['location'] . "/Season " .  $padded;
+				if (!is_dir($dir))
+				{
+					continue;
+				}
+				$series[] = $values['tvdbid'];
+			}
+		}
+
+		$result_series = array_unique($series);
+		// Create a string for trakt and get user Progress
+		$comma_separated = implode(",", $result_series);
+		$buffer = curl("http://api.trakt.tv/user/progress/watched.json/" . $trakt_api . "/" . $trakt_username . "/" . $comma_separated);			
+		$remove_pilots = $result_trakt = json_decode($buffer, true);
+		foreach ($remove_pilots as $x => $y)
 		{
-			$padded = sprintf('%02d', $season); 
-			$dir = $result_show['data']['location'] . "/Season " .  $padded;
-			if (!is_dir($dir))
+			$title = $y['next_episode']['title'];
+			// Check if the episode title contains Pilot
+			// If true we get the first episode data from sickbeard
+			// Trakt api search for Pilot takes too long
+			if (strpos($title, 'Pilot') !== false)
+			{
+				// We remove the result from $result_trakt
+				unset($result_trakt[$x]);
+			}
+		}
+		foreach ($result_trakt as $key => $value)
+		{
+			$tvdbid = $value['show']['tvdb_id'];
+			$title = $value['next_episode']['title'];
+		
+			if ($tvdbid == '0')
+			{
+				$search = curl('http://api.trakt.tv/search/episodes.json/' . $trakt_api . '?query="' . urlencode($title) . '"');
+				$result_search = json_decode($search, true);
+				foreach ($result_search as $k => $v)
+				{
+					$find = $v['show']['tvdb_id'];
+				
+					if (in_array($find, $result_series))
+					{
+						$tvdbid = $find;
+					}
+				}
+			}
+			// We have to remove the tvdbid's from $result_series to get the remaining tvdbid's for the Pilot episodes
+			if(($remove = array_search($tvdbid, $result_series)) !== false)
+			{
+				unset($result_series[$remove]);
+			}
+			// Grab all episode data
+			$episode = curl($sickbeard . "/api/" . $sb_api . "/?cmd=episode&tvdbid=" . $tvdbid . "&season=" . $value['next_episode']['season'] . "&episode=" . $value['next_episode']['number'] . "&full_path=1");
+			$result_eps = json_decode($episode, true);
+			// Remove empty results
+			if ($result_eps['result'] == 'failure')
 			{
 				continue;
 			}
-			$series[] = $values['tvdbid'];
-		}
-	}
-	// Create a string for trakt and get user Progress
-	$comma_separated = implode(",", $series);
-	$buffer = file_get_contents("http://api.trakt.tv/user/progress/watched.json/" . $trakt_api . "/" . $trakt_username . "/" . $comma_separated);			
-	$result_trakt = json_decode($buffer, true);
-	echo "<pre>";
-	foreach ($result_trakt as $key => $value)
-	{
-		$tvdbid = $value['show']['tvdb_id'];
-		if ($tvdbid == '0')
-		{
-			$search = file_get_contents('http://api.trakt.tv/search/episodes.json/' . $trakt_api . '?query="' . urlencode($value['next_episode']['title']) . '"');
-			$result_search = json_decode($search, true);
-			foreach ($result_search as $k => $v)
+			// Put it all in a array
+			// TODO put it in a cache
+			$eps[$tvdbid]['show_name'] = $shows[$tvdbid]['show_name'];
+			$eps[$tvdbid]['episode'] = $value['next_episode']['season'] . 'x' . sprintf('%02d', $value['next_episode']['number']);
+			$eps[$tvdbid]['name'] = $result_eps['data']['name'];
+			$eps[$tvdbid]['status'] = $result_eps['data']['status'];
+			$eps[$tvdbid]['location'] = $result_eps['data']['location'];
+			
+			// Check if there are Dutch subs downloaded for this episode
+			$find_nlsub = str_replace('.mkv', '.nl.srt', $result_eps['data']['location']);
+			if (file_exists($find_nlsub))
 			{
-				$find = $v['show']['tvdb_id'];
-				
-				if (in_array($find, $series))
-				{
-    				$tvdbid = $find;
-				}
+				$eps[$tvdbid]['nlsub'] = true;
+			}
+			else
+			{
+				unset($eps[$tvdbid]);
 			}
 		}
-		// Grab all episode data
-		$episode = file_get_contents($sickbeard . "/api/" . $sb_api . "/?cmd=episode&tvdbid=" . $tvdbid . "&season=" . $value['next_episode']['season'] . "&episode=" . $value['next_episode']['number'] . "&full_path=1");
-		$result_eps = json_decode($episode, true);
-		// Remove empty results
-		if ($result_eps['result'] == 'failure')
+		foreach ($result_series as $c => $d)
 		{
-			continue;
-		}
-		// Put it all in a array
-		// TODO put it in a cache
-		$eps[$tvdbid]['show_name'] = $shows[$tvdbid]['show_name'];
-		$eps[$tvdbid]['episode'] = $value['next_episode']['season'] . 'x' . sprintf('%02d', $value['next_episode']['number']);
-		$eps[$tvdbid]['name'] = $result_eps['data']['name'];
-		$eps[$tvdbid]['status'] = $result_eps['data']['status'];
-		$eps[$tvdbid]['location'] = $result_eps['data']['location'];
+			$pilot = curl($sickbeard . "/api/" . $sb_api . "/?cmd=episode&tvdbid=" . $d . "&season=1&episode=1&full_path=1");
+			$result_pilot = json_decode($pilot, true);
+		
+			// Put it all in a array
+			// TODO put it in a cache
+			$eps[$d]['show_name'] = $shows[$d]['show_name'];
+			$eps[$d]['episode'] = '1x01';
+			$eps[$d]['name'] = $result_pilot['data']['name'];
+			$eps[$d]['status'] = $result_pilot['data']['status'];
+			$eps[$d]['location'] = $result_pilot['data']['location'];
 			
-		// Check if there are Dutch subs downloaded for this episode
-		$find_nlsub = str_replace('.mkv', '.nl.srt', $result_eps['data']['location']);
-		if (file_exists($find_nlsub))
-		{
-			$eps[$tvdbid]['nlsub'] = true;
+			// Check if there are Dutch subs downloaded for this episode
+			$find_nlsub = str_replace('.mkv', '.nl.srt', $result_pilot['data']['location']);
+			if (file_exists($find_nlsub))
+			{
+				$eps[$d]['nlsub'] = true;
+			}
+			else
+			{
+				unset($eps[$d]);
+			}
 		}
-		else
-		{
-			unset($eps[$tvdbid]);
-		}
+		
+		// Save array as json
+		file_put_contents($cache_file, json_encode($eps));
+		$cached = $eps;
 	}
+	else
+	{
+		// Retrieve json and decode to array
+		$cached = json_decode(file_get_contents($cache_file), true);
+	}
+	
 	echo '<div class="container">';
 	echo '<h4>What 2 Watch</h4>';
-	foreach ($eps as $a => $b)
+	foreach ($cached as $a => $b)
 	{
-		echo '<div class="header">' . $b['show_name'] . '</div>';
+		// Lets grab the banner
 		$banner = $sickbeard . "/api/" . $sb_api . "/?cmd=show.getbanner&tvdbid=" . $a;
+		echo '<div class="header">' . $b['show_name'] . '</div>';
 		echo '<div><img src="' . $banner . '" /></div>';
 		echo '<div class="footer">' . $b['episode'] . ' - ' . $b['name'] . '</div>';
 		echo '<br />';
 	}
 	echo '</div>';
-	print_r($eps);
+	print_r($cached);
 	echo "</pre>";
 }
 else
@@ -190,4 +251,13 @@ else
 	echo '</form></div>';
 }
 echo '</body></html>';
+function curl($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
 ?>
