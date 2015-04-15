ed22f8fea4a3141127c74c26ff8b446891719ad0
Mickroz <mike.kros@gmail.com>
2015-04-15 13:47:30 +0200

    Added bootstrap menu and search on log page

--- log.php
+++ log.php
@@ -6,8 +6,29 @@ if (!defined('IN_W2W'))
 
 $error_log = file('error.log', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-$log_lines = '<a href="?mode=viewlog&level=all">All</a> | <a href="?mode=viewlog&level=info">Info</a> | <a href="?mode=viewlog&level=debug">Debug</a>  | <a href="?mode=viewlog&level=error">Error</a> | <a href="?mode=viewlog&level=warning">Warning</a> | <a href="?mode=purge_log">Purge Log</a><br /><br />';
+$log_lines = '<form method="post" action="?mode=viewlog" id="searchform"> 
+	<div class="input-group">
+      <div class="input-group-btn">
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=viewlog&level=all\'">All</button>
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=viewlog&level=info\'">Info</button>
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=viewlog&level=debug\'">Debug</button>
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=viewlog&level=error\'">Error</button>
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=viewlog&level=warning\'">Warning</button>
+		<button type="button" class="btn btn-default" onClick="location.href=\'?mode=purge_log\'">Purge Log</button>
+	</div><!-- /btn-group -->
+	<input type="text" name="message" id="message" maxlength="255" class="form-control" placeholder="Search for...">
+	<span class="input-group-btn">
+        <button type="submit" name="submit" class="btn btn-default"><i class="fa fa-search fa-lg"></i> Search</button>
+      </span>
+	</div><!-- /input-group -->
+	</form>
+	<br />';
+if ($submit)
+{
+	header('Location: index.php?mode=viewlog&search=' . $_POST['message']);
+}
 $level = (isset($_GET['level'])) ? $_GET['level'] : '';
+$search = (isset($_GET['search'])) ? $_GET['search'] : '';
 $view_log = '';
 $error_log = array_reverse($error_log);
 $count = 1;
@@ -17,23 +38,35 @@ foreach ($error_log as $key => $val)
 	{
 		break;
 	}
-	if ($level == '' || $level == 'all')
+	if (!$search)
 	{
-		$view_log .= $val . "\r\n";
-		$count++;
-		continue;
+		if ($level == '' || $level == 'all')
+		{
+			$view_log .= $val . "\r\n";
+			$count++;
+			continue;
+		}
+		if (strpos($val, strtoupper($level)) !== false)
+		{
+			$view_log .= $val . "\r\n";
+			$count++;
+		}
 	}
-	if (strpos($val, strtoupper($level)) !== false)
+	if ($search)
 	{
-		$view_log .= $val . "\r\n";
-		$count++;
+		if (strpos($val, $search) !== false)
+		{
+			$view_log .= $val . "\r\n";
+			$count++;
+		}
 	}
 }
 $log_lines .= '<div class="code"><strong>' . (($level == '') ?  'All' : ucfirst($level)) . '</strong></div>';
 $log_lines .= '<pre>';
 if (empty($view_log))
 {
-	$log_lines .= "There is currently no $level information in your log file!";
+	$info = (!empty($level) ? $level : $search);
+	$log_lines .= "There is currently no $info information in your log file!";
 }
 else
 {
